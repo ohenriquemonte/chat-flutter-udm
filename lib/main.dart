a40bc0874e7f930b6b8d5443a6178b33ab1dc9ac
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-29 17:40:10 -0300

    119. Lendo Dados no Banco

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,46 @@ import 'package:flutter/material.dart';
 void main() async {
   runApp(MyApp());
 
-  Firestore.instance
-      .collection('col')
-      .document('doc')
-      .setData({'texto': 'henrique'});
+    // Firestore.instance.collection('col').document('doc').setData({'texto': 'henrique'});
+
+    // Firestore.instance.collection('mensagens').document().setData({
+    //     'texto': 'Olá',
+    //     'from': 'Henrique'
+    //   });
+
+    // Firestore.instance.collection('mensagens').document().setData({
+    //   'texto': 'Tudo bem?',
+    //   'from': 'Daniel'
+    // });
+
+    // Firestore.instance.collection('mensagens').document().setData({
+    //   'texto': 'Tudo ótimo',
+    //   'from': 'Henrique'
+    // });
+
+  // QuerySnapshot snapshot = await Firestore.instance.collection('mensagens').getDocuments(); 
+  
+  // snapshot.documents.forEach((d) {
+  //    d.reference.updateData({
+  //      'lido': false
+  //    });
+  //    print(d.documentID);
+  //    print(d.data);
+  // });
+  
+  // DocumentSnapshot snapshot1 = await Firestore.instance.collection('mensagens').document('D11Tpijrshti7KerTx4K').get(); 
+  // print(snapshot1.data);
+
+  // Firestore.instance.collection('mensagens').snapshots().listen((dado) {
+  //   dado.documents.forEach((d) {
+  //     print(d.data);
+  //   });
+  //   print(dado.documents[0].data);
+  // });
+
+  Firestore.instance.collection('mensagens').document('D11Tpijrshti7KerTx4K').snapshots().listen((dado2) {
+    print(dado2.data);
+  });
 }
 
 class MyApp extends StatelessWidget {
