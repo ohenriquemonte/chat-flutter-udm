50e63913ba1ca1c9bb01cc7893e463f84a01ee9b
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-05 12:08:42 -0300

    121. 2 - Enviando o Texto para o Firebase

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,20 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:chat/text_composer.dart';
 
 class ChatScreen extends StatefulWidget {
-    @override
-    _ChatScreenState createState() => _ChatScreenState();
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Olá'),
-                elevation: 0,
-            ),
-            body: TextComposer(),
-        );
-    }
+  void _sendMessage(String text) {
+    Firestore.instance.collection('messages').add({'text': text});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Olá'),
+        elevation: 0,
+      ),
+      body: TextComposer(_sendMessage),
+    );
+  }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,46 +1,61 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
-    @override
-    _TextComposerState createState() => _TextComposerState();
+  TextComposer(this.sendMessage);
+  Function(String) sendMessage;
+
+  @override
+  _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
-    bool _isComposing = false;
+  final TextEditingController _controller = TextEditingController();
+  bool _isComposing = false;
 
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            margin: const EdgeInsets.symmetric(
-                horizontal: 8,
-            ),
-            child: Row(
-                children: [
-                    IconButton(
-                        icon: Icon(Icons.photo_camera),
-                        onPressed: () {
+  void _reset() {
+    _controller.clear();
 
-                        },
-                    ),
-                    Expanded(
-                        child: TextField(
-                            decoration: InputDecoration.collapsed(hintText: 'Enviar uma mensagem'),
-                            onChanged: (text){
-                                setState(() {
-                                    _isComposing = text.isNotEmpty;
-                                });
-                            },
-                            onSubmitted: (text) {}
-                        ),
-                    ),
-                    IconButton(
-                        icon: Icon(Icons.send, color: Colors.orange),
-                        onPressed: _isComposing ? () {
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
-                        }: null,
-                    ),
-                ]
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8,
+      ),
+      child: Row(children: [
+        IconButton(
+          icon: Icon(Icons.photo_camera),
+          onPressed: () {},
+        ),
+        Expanded(
+          child: TextField(
+              controller: _controller,
+              decoration:
+                  InputDecoration.collapsed(hintText: 'Enviar uma mensagem'),
+              onChanged: (text) {
+                setState(() {
+                  _isComposing = text.isNotEmpty;
+                });
+              },
+              onSubmitted: (text) {
+                widget.sendMessage(text);
+                _reset();
+              }),
+        ),
+        IconButton(
+          icon: Icon(Icons.send, color: Colors.orange),
+          onPressed: _isComposing
+              ? () {
+                  widget.sendMessage(_controller.text);
+                  _reset();
+                }
+              : null,
+        ),
+      ]),
+    );
+  }
 }
